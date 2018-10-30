927823206204e356ee763920d503bb9e1254e8f8
Ramón Lozano <niksokupa@gmail.com>
2018-10-30 23:53:10 +0100

    Rutas hechas

--- public_html/js/system/config/app.js
+++ public_html/js/system/config/app.js
@@ -4,10 +4,18 @@ var trolleyes = angular.module('MyApp', [
     'ngRoute',
     'services',
     'commonControllers',
-    'tipousuarioControllers'
+    'tipousuarioControllers',
+    'usuarioControllers',
+    'tipoproductoControllers',
+    'facturaControllers',
+    'productoControllers'
 ]);
 
 
 var moduleCommon = angular.module ('commonControllers',[]);
 var moduleService = angular.module ('services',[]);
 var moduleTipousuario = angular.module ('tipousuarioControllers',[]);
+var moduleUsuario = angular.module ('usuarioControllers',[]);
+var moduleProducto = angular.module ('productoControllers',[]);
+var moduleFactura = angular.module ('facturaControllers',[]);
+var moduleTipoproducto = angular.module('tipoproductoControllers',[]);
--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -2,5 +2,9 @@ trolleyes.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {templateUrl:'js/app/common/home.html', controller: 'homeController'});
         $routeProvider.when('/tipousuario/plist', {templateUrl:'js/app/tipousuario/plist.html', controller: 'tipousuarioPlistController'});
         $routeProvider.when('/tipousuario/edit/:id?', {templateUrl:'js/app/tipousuario/edit.html', controller: 'tipousuarioEditController'});
+        $routeProvider.when('/usuario/plist', {templateUrl:'js/app/usuario/plist.html', controller: 'usuarioPlistController'});
+        $routeProvider.when('/factura/plist', {templateUrl:'js/app/factura/plist.html', controller: 'facturaPlistController'});
+        $routeProvider.when('/tipoproducto/plist', {templateUrl:'js/app/tipoproducto/plist.html', controller: 'tipoproductoPlistController'});
+        $routeProvider.when('/producto/plist', {templateUrl:'js/app/producto/plist.html', controller: 'productoPlistController'});
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
