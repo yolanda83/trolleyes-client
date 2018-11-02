063fcff3b9971e1bbed42d8cb8856d4f5c94e606
Ramón Lozano <niksokupa@gmail.com>
2018-11-02 20:55:47 +0100

    Desplegable RPP funcionando. (1/3) (No funciona ng-init ni p'atrás)

--- public_html/js/app/usuario/plist.js
+++ public_html/js/app/usuario/plist.js
@@ -4,7 +4,6 @@ moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$locatio
     function ($scope, $http, $location, toolService, $routeParams) {
 
         $scope.totalPages = 1;
-
         if (!$routeParams.rpp) {
             $scope.rpp = 10;
         } else {
@@ -20,39 +19,6 @@ moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$locatio
                 $scope.page = 1;
             }
         }
-
-        $scope.reload = function () {
-            $http({
-                method: 'GET',
-                url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=getpage&rpp=' + $scope.selectedItem + '&page=' + 1
-            }).then(function (response) {
-                $scope.status = response.status;
-                $scope.ajaxDataUsuarios = response.data.message;
-            }, function (response) {
-                $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
-                $scope.status = response.status;
-            });
-
-            $http({
-                method: 'GET',
-                url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=getcount'
-            }).then(function (response) {
-                $scope.status = response.status;
-                $scope.ajaxDataUsuariosNumber = response.data.message;
-                $scope.totalPages = Math.ceil($scope.ajaxDataUsuariosNumber / $scope.selectedItem);
-                $scope.list = [];
-                for (var i = 1; i <= $scope.totalPages; i++) {
-                    $scope.list.push(i);
-                }
-            }, function (response) {
-                $scope.ajaxDataUsuariosNumber = response.data.message || 'Request failed';
-                $scope.status = response.status;
-            });
-
-
-        }
-
-
         $http({
             method: 'GET',
             url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=getcount'
@@ -79,6 +45,21 @@ moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$locatio
             $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
             $scope.status = response.status;
         });
+
+        $scope.update = function () {
+            $http({
+                method: 'GET',
+                url: `http://localhost:8081/trolleyes/json?ob=usuario&op=getpage&rpp=${$scope.selectedItem}&page=1`
+            }).then(function (response) {
+                $location.url(`usuario/plist/${$scope.selectedItem}/1`);
+                $scope.status = response.status;
+                $scope.ajaxDataUsuarios = response.data.message;
+            }, function (response) {
+                $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
+                $scope.status = response.status;
+            });
+        }
+
         $scope.isActive = toolService.isActive;
     }
 ]);
