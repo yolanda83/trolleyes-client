a29b56e56a782d30471d6d8ff5b95ddd4c061c24
a047692839q <a047692839q@s09-pc06.ausias.lan>
2018-12-11 17:22:47 +0100

    Arreglado problema con el refresco de página y la sesión

--- public_html/js/system/config/run.js
+++ public_html/js/system/config/run.js
@@ -9,18 +9,18 @@ trolleyes.run(['$rootScope', 'sessionService', '$location', '$http', 'countcarri
                 url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=check'
             }).then(function (response) {
                 if (response.data.status === 200) {
-                    oSessionService.setSessionActive;
+                    oSessionService.setSessionActive();
                     oSessionService.setUserName(response.data.message.login);
                     oSessionService.setId(response.data.message.id);
                 } else {
-                    oSessionService.setSessionInactive;
+                    oSessionService.setSessionInactive();
                     if (nextUrl !== '/home' && nextUrl !== '/usuario/login/'
                             && nextUrl != '/usuario/login') {
                         $location.path("/home");
                     }
                 }
             }, function (response) {
-                oSessionService.setSessionInactive;
+                oSessionService.setSessionInactive();
                 if (nextUrl !== '/home' && nextUrl !== '/usuario/login/' 
                         && nextUrl !== '/usuario/login') {
                     $location.path("/home");
